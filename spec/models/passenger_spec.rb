737d5ebe5ee3324f9d0c66701787ece85b11b87f
Leah Riffell <leah.riffell@gmail.com>
2020-09-21 11:18:00 -0600

    Add test and method for total flights a passenger has been on

--- spec/models/passenger_spec.rb
+++ spec/models/passenger_spec.rb
@@ -5,4 +5,24 @@ RSpec.describe Passenger, type: :model do
     it {should have_many(:flights).through(:flight_passengers)}
     it {should have_many :flight_passengers}
   end
+
+  describe 'instance methods' do
+    before :each do 
+      airline = Airline.create!(name: "Southwest Airlines")
+
+      flight_1 = airline.flights.create!(number: "A123")
+      flight_2 = airline.flights.create!(number: "B456")
+
+      @passenger_1 = flight_1.passengers.create!(name: "Harry Potter", age: 17)
+      @passenger_2 = flight_1.passengers.create!(name: "Snoopy", age: 10)
+      flight_2.passengers << @passenger_1
+    end
+
+    describe 'it can count the num of flights a passenger has been on' do
+      it "#flight_count" do
+        expect(@passenger_1.flight_count).to eq(2)
+        expect(@passenger_2.flight_count).to eq(1)
+      end
+    end
+  end
 end

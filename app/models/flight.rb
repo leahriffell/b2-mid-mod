215f8fc3b449850f94ef27fa406b11f0a83558ec
Leah Riffell <leah.riffell@gmail.com>
2020-09-21 10:07:28 -0600

    Add test and method for getting list of flight's passengers' names

--- app/models/flight.rb
+++ app/models/flight.rb
@@ -1,5 +1,9 @@
 class Flight < ApplicationRecord
   belongs_to :airline
-  has_many :passengers, through: :flight_passengers
   has_many :flight_passengers
+  has_many :passengers, through: :flight_passengers
+
+  def passenger_names
+    passengers.pluck(:name)
+  end
 end
--- spec/models/flight_spec.rb
+++ spec/models/flight_spec.rb
@@ -6,4 +6,21 @@ RSpec.describe Flight, type: :model do
     it {should have_many(:passengers).through(:flight_passengers)}
     it {should have_many :flight_passengers}
   end
+
+  describe 'instance methods' do 
+    before :each do 
+      airline = Airline.create!(name: "Southwest Airlines")
+
+      @flight_1 = airline.flights.create!(number: "A123")
+
+      @passenger_1 = @flight_1.passengers.create!(name: "Harry Potter", age: 17)
+      @passenger_2 = @flight_1.passengers.create!(name: "Snoopy", age: 10)
+    end
+
+    describe 'it can return an array of all passenger names' do 
+      it "#passenger_names" do 
+        expect(@flight_1.passenger_names).to eq([@passenger_1.name, @passenger_2.name])
+      end
+    end
+  end
 end

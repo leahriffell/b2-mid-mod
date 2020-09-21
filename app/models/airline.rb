ff9f9f811606e45ce0ef8da933b49a7d323ecd3a
Leah Riffell <leah.riffell@gmail.com>
2020-09-21 10:28:16 -0600

    Add test and method for getting unique pass names on airline

--- app/models/airline.rb
+++ app/models/airline.rb
@@ -1,3 +1,8 @@
 class Airline < ApplicationRecord
   has_many :flights
+  has_many :passengers, through: :flights
+
+  def passenger_names
+    passengers.distinct.order(:name).pluck(:name)
+  end
 end
--- spec/models/airline_spec.rb
+++ spec/models/airline_spec.rb
@@ -4,4 +4,24 @@ RSpec.describe Airline, type: :model do
   describe 'relationships' do
     it {should have_many :flights}
   end
+
+  describe 'instance methods' do 
+    before :each do
+      @airline = Airline.create!(name: "Southwest Airlines")
+
+      @flight_1 = @airline.flights.create!(number: "A123")
+      @flight_2 = @airline.flights.create!(number: "B456")
+
+      @passenger_1 = @flight_1.passengers.create!(name: "Harry Potter", age: 17)
+      @passenger_2 = @flight_1.passengers.create!(name: "Snoopy", age: 10)
+      @passenger_3 = @flight_2.passengers.create!(name: "Elmer Dawg", age: 5)
+      @flight_2.passengers << @passenger_1
+    end
+
+    describe "it can return an array of all unique passengers' names" do 
+      it "#passenger_names" do
+        expect(@airline.passenger_names).to eq([@passenger_1.name, @passenger_2.name, @passenger_3.name].sort)
+      end
+    end
+  end
 end
